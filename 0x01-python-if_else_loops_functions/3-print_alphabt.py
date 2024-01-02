73195bc5619d0e2454f9ce78c5a789b23f304469
Isaiah woko <wokoisaiah68@gmail.com>
2024-01-02 15:39:25 +0100

    changes

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for alph in range(ord('a'), ord('z')):
-    if chr(alph) not in ['e', 'q']:
+    if chr(alph) != 'e' and chr(alph) != 'q':
         print("{}".format(chr(alph)), end="")
