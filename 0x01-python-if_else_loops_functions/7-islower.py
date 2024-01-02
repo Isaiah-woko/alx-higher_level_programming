9d4c01caf60837d858948be502fca5c27e3afb26
Isaiah woko <wokoisaiah68@gmail.com>
2024-01-02 23:41:42 +0100

    corrections

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def islower(c):
-    for char in c:
-        if ord(char) in range(97, 123):
+        if ord(c) in range(97, 123):
             return True
         else:
             return False
