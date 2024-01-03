a5be0fb94b5b5a959404d2fe9df138a378c59b65
Isaiah woko <wokoisaiah68@gmail.com>
2024-01-03 19:35:35 +0100

    6-print_comb3.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 for i in range(0, 9):
     for j in range(i + 1, 10):
-        print(str(i % 10) + str(j % 10), end='')
+        print("{}{}".format(i % 10, j % 10), end='')
 
         if i != 8 or j != 9:
             print(", ", end='')
