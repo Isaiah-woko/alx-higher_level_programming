a3de1ae783014aa191876df7e397470a68362df1
Isaiah woko <wokoisaiah68@gmail.com>
2024-01-04 13:10:50 +0100

    fixed task 0

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -2,11 +2,13 @@
 import add_0
 
 
-def main():
+def add_number():
     a = 1
     b = 2
+
     result = add_0.add(a, b)
     print("{} + {} = {}".format(a, b, result))
 
-    if __name__ == "__main__":
-        main()
+
+if __name__ == "__main__":
+    add_number()
