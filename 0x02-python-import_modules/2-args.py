af207b411f8d0ee3fe0209423fa0a6660804ca61
Isaiah woko <wokoisaiah68@gmail.com>
2024-01-04 14:53:51 +0100

    2-args.py

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    from sys import argv
+
+    arguments = argv[0:]
+
+    if len(arguments) == 0:
+        print("{} {}".format(len(argv), "arguments."))
+    elif len(arguments) == 1:
+        print("{} {}".format(len(argv), "argument:"))
+    else:
+        print("{} {}".format(len(argv) - 1, "arguments:"))
+
+for i in range(1, len(arguments)):
+    print("{}: {}".format(i, arguments[i]))
