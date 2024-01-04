e513a01cb21519a747fcef7cfa795a6c68adcba6
Isaiah woko <wokoisaiah68@gmail.com>
2024-01-04 17:16:56 +0100

    made changes

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,19 +1,10 @@
 #!/usr/bin/python3
+
 if __name__ == "__main__":
     import hidden_4
 
     list_of_names = dir(hidden_4)
 
-    for list in range(len(list_of_names)):
-        if not list_of_names[list] .startwith("__"):
-            print(list_of_names[list])
-
-
-# if __name__ == "__main__":
-#     import hidden_4
-
-#     name_list = dir(hidden_4)
-
-#     for name in range(len(name_list)):
-#         if not name_list[name].startswith('__'):
-#             print(name_list[name])
+    for name in list_of_names:
+        if not name.startswith("__"):
+            print(name)
