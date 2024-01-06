7cc77ef40173b2d37fbaf6d5d171c280b25ad141
Isaiah woko <wokoisaiah68@gmail.com>
2024-01-06 20:33:56 +0100

    made changes

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
-    for print_list in my_list:
-        print("{}".format(print_list))
+    for num in my_list:
+        print("{}".format(num))
