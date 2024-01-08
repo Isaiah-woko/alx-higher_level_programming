3afb8ba2fb935e86dd3b1f224f3ad52f6f249935
Isaiah woko <wokoisaiah68@gmail.com>
2024-01-08 10:35:23 +0100

    5-no_c.py

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,6 +2,6 @@
 def no_c(my_string):
     new_string = ""
     for character in my_string:
-        if character.lower() != 'c':
+        if character != 'c' and character != 'C':
             new_string += character
     return new_string
