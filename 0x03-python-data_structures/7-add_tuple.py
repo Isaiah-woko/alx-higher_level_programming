4988529ced6840e5f5dcaf63f0c92afc647b1249
Isaiah woko <wokoisaiah68@gmail.com>
2024-01-08 16:56:08 +0100

    7-add_tuple.py

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    while len(tuple_a) < 2:
+        tuple_a += (0,)
+    while len(tuple_b) < 2:
+        tuple_b += (0,)
+    first_element = tuple_a[0] + tuple_b[0]
+    second_element = tuple_a[1] + tuple_b[1]
+
+    new_tuple = (first_element, second_element)
+    return new_tuple
