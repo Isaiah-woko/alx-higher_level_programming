0dc57a84ac4a2383a21f3272bd3f9bde4b8a9331
Isaiah woko <wokoisaiah68@gmail.com>
2024-02-05 15:17:51 +0100

    corrections

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -3,4 +3,8 @@
 """This module is for the function that returns attributes"""
 
 def lookup(obj):
+
+    """Thiz function returns a list of all
+       the objects attributs and methods
+    """
     return dir(obj)
