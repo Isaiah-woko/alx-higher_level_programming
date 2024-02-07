daeba8ca66bf6ceabba60391d404a0de44fc15ca
Isaiah woko <wokoisaiah68@gmail.com>
2024-02-07 17:46:36 +0100

    corrections

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -49,8 +49,11 @@ class Square(Rectangle):
     """
 
     def __init__(self, size):
-        super().__init__(size, size)
         self.integer_validator("size", size)
+        self.__size = size
+
+    def __str__(self):
+        return "[Rectangle] {}/{}".format(self.__size, self.__size)
 
     def area(self):
-        return super().area()
+        return (self.__size ** 2)
