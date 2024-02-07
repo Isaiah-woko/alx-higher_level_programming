4c7371298e3a5540eee021c6b5d745b858f6fa10
Isaiah woko <wokoisaiah68@gmail.com>
2024-02-07 21:59:20 +0100

    added 0-read_file.py

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""This module contains the code for file input"""
+
+
+def read_file(filename=""):
+    """
+        This is a function for opening a file using with
+        Args:
+        filename: The file to open
+    """
+    with open(filename, encoding="utf-8") as f:
+        read_data = f.read()
+        print(read_data)
