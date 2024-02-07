581f49de43556f041fe3139201e106660507c2b9
Isaiah woko <wokoisaiah68@gmail.com>
2024-02-07 18:59:34 +0100

    correxctions

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -5,6 +5,15 @@
 
 def add_attribute(obj, attribute_name, attribute_value):
 
+    """
+    This function dynamically adds a new attribute
+
+    Args:
+        obj: this is an object of the class for the new attribute.
+        Attribute_name: the attribute name
+        Attribute_value: the attribute value
+    """
+
     if not attribute_name or not attribute_value:
         raise TypeError("can't add new attribute")
     if not hasattr(obj, '__dict__'):
