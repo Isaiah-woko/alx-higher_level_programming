f69ed5da2d61bf555a12a4cde8f90b185188fe74
Isaiah woko <wokoisaiah68@gmail.com>
2024-02-07 16:45:18 +0100

    modified test case

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+"""This module is an empty class"""
+
+class Rectangle(BaseGeometry):
+    def __init__(self, width, heigt):
+        self.__width = width
+        self.__height = __height
