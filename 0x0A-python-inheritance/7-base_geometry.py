0f7f4e03d4883aaab8ba518ac9409ced4f0f8ff3
Isaiah woko <wokoisaiah68@gmail.com>
2024-02-07 18:17:34 +0100

    okay

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -15,10 +15,7 @@ class BaseGeometry:
     def integer_validator(self, name, value):
 
         if not isinstance(value, int):
-            raise TypeError(f"{name} must be an integer")
+            raise TypeError("{} must be an integer".format(name))
 
         if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
-
-        if not isinstance(name, str):
-            raise TypeError("{} must be a string".format(name))
+            raise ValueError("{} must be greater than 0".format(name))
--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -3,25 +3,7 @@
 """This module contains one class and one subclass"""
 
 
-class BaseGeometry:
-    """This class respresnets the definition for the BaseGeometry class
-        It has several methods thats defines the Area and some other properties
-        of the BaseGeometry class object.
-    """
-
-    def area(self):
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-
-        if not isinstance(value, int):
-            raise TypeError(f"{name} must be an integer")
-
-        if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
-
-        if not isinstance(name, str):
-            raise TypeError("{} must be a string".format(name))
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
@@ -29,9 +11,8 @@ class Rectangle(BaseGeometry):
        methods of the BaseGeometry class.
     """
     def __init__(self, width, height):
-
-        self.__height = height
-        self.__width = width
-
+        super().__init__()
         self.integer_validator("width", width)
         self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
