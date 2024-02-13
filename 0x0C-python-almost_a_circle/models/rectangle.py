721a6e37a8be0fe670712f5b866794126d591f27
Isaiah woko <wokoisaiah68@gmail.com>
2024-02-13 02:13:22 +0100

    updated

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -24,7 +24,7 @@ class Rectangle(Base):
 
         # validate width attribute.
         if not isinstance(width, int):
-            raise TypeError("width must be an intger")
+            raise TypeError("width must be an integer")
         elif width <= 0:
             raise ValueError("width must be > 0")
 
