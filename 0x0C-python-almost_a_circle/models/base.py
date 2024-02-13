fc17488d54d580502d305ed447b1a505e951cfb7
Isaiah woko <wokoisaiah68@gmail.com>
2024-02-13 01:38:05 +0100

    updates

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -26,7 +26,7 @@ class Base(object):
     def to_json_string(list_dictionaries):
         """returns the JSON string representation"""
 
-        if not list_dictionaries:
+        if list_dictionaries is None or list_dictionaries == []:
             return []
 
         return json.dumps(list_dictionaries)
