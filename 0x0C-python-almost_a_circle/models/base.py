1927ddb2c634962287af537272fcb6bb1444284c
Isaiah woko <wokoisaiah68@gmail.com>
2024-02-13 01:48:06 +0100

    okay

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -33,12 +33,33 @@ class Base(object):
 
     @classmethod
     def save_to_file(cls, list_objs):
-        """Save objects to a JSON file."""
-        filename = f"{cls.__name__}.json"
-        json_objs = [obj.to_dictionary() for obj in list_objs]
+        """this method saves to file"""
+        cls.json_obj_list = []
+        cls.filename = "{}.json".format(cls.__name__)
 
-        with open(filename, "w") as file:
-            if list_objs is None:
-                file.write("[]")
-            else:
-                file.write(cls.to_json_string(json_objs))
+        for cls_obj in list_objs:
+            cls.json_obj_list.append(cls_obj.to_dictionary())
+
+        with open(cls.filename, "w") as file:
+            file.write(cls.to_json_string(cls.json_obj_list))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """converts from json"""
+        if json_string is None:
+            return []
+        else:
+            return json_string
+
+    @classmethod
+    def create(cls, **dictionary):
+        """creates a new instance"""
+        if cls.__name__ == "Rectangle":
+            instance = cls(5, 10)
+        elif cls.__name__ == "Square":
+            instance = cls(7)
+        else:
+            raise TypeError("unknow")
+        instance.update(**dictionary)
+
+        return instance
