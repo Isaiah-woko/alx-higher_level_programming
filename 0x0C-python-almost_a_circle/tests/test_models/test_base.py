8b8b4e231720b7d7dbc1e5810ae28f2d4d761ce0
Isaiah woko <wokoisaiah68@gmail.com>
2024-02-13 02:18:15 +0100

    added tests

---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+
+""" This is the module for the test cases."""
+
+
+import unittest
+from models.base import Base
+
+
+class TestBaseClass(unittest.TestCase):
+
+    def test_id_not_provided(self):
+        obj1 = Base()
+        self.assertEqual(obj1.id, 2)
+
+        obj2 = Base()
+        self.assertEqual(obj2.id, 2)
+
+    def test_id_provided(self):
+        obj3 = Base(id=10)
+        self.assertEqual(obj3.id, 10)
+
+    def test_class_attribute_increment(self):
+        obj4 = Base()
+        obj5 = Base()
+        self.assertEqual(obj5.id, obj4.id + 1)
