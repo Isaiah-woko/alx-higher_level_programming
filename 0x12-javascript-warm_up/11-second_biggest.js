772673701f2be4e38ba72fb4f5415a5b695d76fd
Isaiah woko <wokoisaiah68@gmail.com>
2024-06-20 02:54:51 +0100

    added 11-second_biggest.js

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,1 +1,10 @@
 #!/usr/bin/node
+const arg = process.argv.slice(2).map(Number);
+
+if (arg.length < 2) {
+  console.log(0);
+} else {
+  const sortedArgs = arg.sort((a, b) => b - a);
+  const secondBiggest = sortedArgs[1];
+  console.log(secondBiggest);
+}
