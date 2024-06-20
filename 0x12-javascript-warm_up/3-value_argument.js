c62296ae627f37498e47f7c175ab0c18eaf64460
Isaiah woko <wokoisaiah68@gmail.com>
2024-06-20 02:46:12 +0100

    correction

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -2,9 +2,7 @@
 if (process.argv[2] === undefined) {
   console.log('No argument');
 } else {
-  let result = '';
   for (let i = 2; process.argv[i] !== undefined; i++) {
-    result += process.argv[i] + ' ';
+    console.log(process.argv[i]);
   }
-  console.log(result);
 }
