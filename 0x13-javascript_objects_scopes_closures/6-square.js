0b1a5b81b8a3a91c2689f1e2aceda4971ce51877
Isaiah woko <wokoisaiah68@gmail.com>
2024-06-21 01:37:00 +0100

    added 6-square.js

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+const SecondSquare = require('./5-square.js');
+class Square extends SecondSquare {
+  charPrint (c) {
+    if (c === undefined) {
+      this.print();
+    } else {
+      for (let i = 0; i < this.height; i++) {
+        let row = '';
+        for (let j = 0; j < this.width; j++) {
+          row += 'C';
+        }
+        console.log(row);
+      }
+    }
+  }
+}
+
+module.exports = Square;
