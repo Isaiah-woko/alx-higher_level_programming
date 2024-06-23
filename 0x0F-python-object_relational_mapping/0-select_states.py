0224e80d4009dd53009a3dcd96a0773e8691b1db
Isaiah woko <wokoisaiah68@gmail.com>
2024-06-23 20:33:47 +0100

    okay

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -9,7 +9,8 @@ if __name__ == "__main__":
         user=sys.argv[1],
         passwd=sys.argv[2],
         db=sys.argv[3],
-        port=3306
+        port=3306,
+        charset="utf8"
     )
 
     cursor = db.cursor()
