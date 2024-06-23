2373d3d6a11a1bf613e1f06c8fff42d25342814b
Isaiah woko <wokoisaiah68@gmail.com>
2024-06-23 20:08:15 +0100

    okay

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
         host="localhost",
         user=sys.argv[1],
         passwd=sys.argv[2],
-        database=sys.argv[3],
+        db=sys.argv[3],
         port=3306
     )
 
