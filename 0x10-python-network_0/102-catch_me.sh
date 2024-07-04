e768c72bb412e0eba4575cc3c5deab7be9f084ec
Isaiah woko <wokoisaiah68@gmail.com>
2024-07-04 20:07:18 +0100

    okay

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 # Catch me if you can!
-curl -s -L -X PUT "0.0.0.0:5000/catch_me" -d "user_id=98"
+curl -s -L -X PUT -d "user_id=98" "0.0.0.0:5000/catch_me" | grep -q "You got me!" && echo "You got me!"
+
