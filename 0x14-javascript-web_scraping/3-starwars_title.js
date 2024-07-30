8cc4c32e8c65df80a6a64047871072709b0c4dcc
Isaiah woko <wokoisaiah68@gmail.com>
2024-07-30 23:36:35 +0100

    task 3

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+const request = require('request');
+const movieId = process.argv[2];
+const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+
+request(url, { json: true }, (error, response, body) => {
+  if (error) { console.log(error); } else if (response.statusCode === 200) { console.log(body.title); }
+});
