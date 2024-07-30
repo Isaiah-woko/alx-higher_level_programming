8d44e2ca0e6bc33e8d26bb22f3121fb0697be7f0
Isaiah woko <wokoisaiah68@gmail.com>
2024-07-30 22:58:17 +0100

    task 1

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,13 +1,14 @@
 #!/usr/bin/node
 const args = process.argv.slice(2);
 const fileName = args[0];
+const content = args[1];
 
 const fs = require('fs');
 
-fs.readFile(fileName, 'utf8', (err, data) => {
+fs.writeFile(fileName, content, 'utf8', err => {
   if (err) {
     console.error(err);
     return;
   }
-  console.log(data);
+  console.log('Successfull');
 });
