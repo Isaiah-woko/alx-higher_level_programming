9ab729727537024d70eb8c25f1d70915cb06d34e
Isaiah woko <wokoisaiah68@gmail.com>
2024-07-31 00:35:18 +0100

    task 7

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,15 +1,17 @@
 #!/usr/bin/node
-// print the number of movie of a character present
-// with id = 18
+
 const request = require('request');
-const url = process.argv[2];
-const charId = 18;
-request.get(url, { json: true }, (error, response, body) => {
+
+const characterId = 18;
+const filmUrl = process.argv[2];
+const characterUrl = `https://swapi-api.alx-tools.com/api/people/${characterId}/`;
+
+request(filmUrl, { json: true }, (error, response, body) => {
   if (error) {
-    console.log(error);
+    console.error(error);
   } else if (response.statusCode === 200) {
     const films = body.results;
-    const count = films.filter(film => film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${charId}/`)).length;
-    console.log(count);
+    const counter = films.filter(film => film.characters.includes(characterUrl)).length;
+    console.log(counter);
   }
 });
