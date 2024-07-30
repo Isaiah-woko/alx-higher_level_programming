a1718bb83489f678039b0c758128b0e2b977124c
Isaiah woko <wokoisaiah68@gmail.com>
2024-07-30 23:16:46 +0100

    task 2

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -4,11 +4,6 @@ const request = require('request');
 const args = process.argv.slice(2);
 const url = args[0];
 
-// if (!url) {
-//   console.error('Usage: ./status_code.js <URL>');
-//   process.exit(1);
-// }
-
 request(url, (error, response) => {
   if (error) {
     console.error(`Error: ${error}`);
